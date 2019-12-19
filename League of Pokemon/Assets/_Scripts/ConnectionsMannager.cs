3454171ad92e4543e038d4656adb25dbbd9bf73b
Woszu <kruser757@gmail.com>
2019-12-19 13:56:42 +0100

    Menu and PhotonNetwork

---
+++ League of Pokemon/Assets/_Scripts/ConnectionsMannager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionsMannager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ConnectToGameServer()
+    {
+        Debug.Log("Łączę");
+        PhotonNetwork.ConnectUsingSettings("LoP_1.0");
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 20), PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
---
+++ League of Pokemon/Assets/_Scripts/MenuSC.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSC : MonoBehaviour
+{
+    public int icon;
+    public Sprite[] icons;
+    public Image iconButton;
+
+    public GameObject iconsPanel;
+    public InputField nickField;
+    public Slider sl;
+
+    public ConnectionsMannager cm;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ConnectPlay()
+    {
+        if (nickField.text.Length > 3)
+        {
+            cm.ConnectToGameServer();
+        }
+    }
+    
+    void Update()
+    {
+        if (sl.value > 0.35f && sl.value < 0.65f)
+            sl.value = 0.5f;
+        if (sl.value < 0.35f)
+            sl.value = 0.0f;
+        if (sl.value > 0.65f)
+            sl.value = 1.0f;
+        iconButton.sprite = icons[icon];
+    }
+
+    public void IconsPanelButton()
+    {
+        iconsPanel.active = true;
+    }
+
+    public void ChangeIcone(int id)
+    {
+        icon = id;
+        iconsPanel.active = false;
+    }
+}
